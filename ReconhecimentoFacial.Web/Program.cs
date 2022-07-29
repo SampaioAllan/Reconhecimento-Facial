94e4fe3b8b05c4993529daded3b112a3f68937f5
SampaioAllan <lanbrendo93@gmail.com>
2022-07-28 23:59:33 -0300

    reorganizando as responsabilidades das camadas

--- ReconhecimentoFacial.Web/Program.cs
+++ ReconhecimentoFacial.Web/Program.cs
@@ -11,6 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 var injecao = new Injecao();
 injecao.AdicionarServicos(builder);
 
+builder.Services.AddControllers()
+                            .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling =
+                            Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
